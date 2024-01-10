ae301104182984e1957d415a5c7105c9bd07e6c0
ung nguyen <ung.nguyen@datx.com.vn>
2024-01-10 14:34:35 +0700

    refactor chat socket

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ interface ServerToClientEvents {
 }
 
 interface ClientToServerEvents {
-    chat: (id: string) => void
     sendMessage: (message: string) => void
 }
 
@@ -15,12 +14,12 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>()
 new EventLogger(io).config()
 
 io.on('connection', (socket) => {
-    // init socket chat dialog
-    socket.on('chat', (dialogId) => {
-        socket.join(dialogId)
-        socket.on('sendMessage', (message) => {
-            socket.to(dialogId).emit('receiveMessage', message)
-        })
+    const dialogId = socket.handshake.headers.dialogId as string
+
+    socket.join(dialogId)
+
+    socket.on('sendMessage', (message) => {
+        io.to(dialogId).emit('receiveMessage', message)
     })
 })
 
