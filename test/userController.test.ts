c105c975ca075dae3c169a35937275b3098f6e41
stilllove1511 <100393523+stilllove1511@users.noreply.github.com>
2024-02-02 04:07:57 +0000

    Add userController.test.ts and remove test.test.ts

---
+++ test/userController.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect } from '@jest/globals'
+import { UserController } from '@src/controller/user.controller'
+import { UserService } from '@src/service/user.service'
+
+describe('userController', () => {
+    it('search user', async () => {
+        const userService = new UserService(undefined)
+        const userController = new UserController(userService)
+        const key = 'test'
+        const listUser = [
+            {
+                id: 'test1',
+            },
+            {
+                id: 'test2',
+            },
+            {
+                id: 'test3',
+            },
+            {
+                id: '2132',
+            },
+            {
+                id: '4342',
+            },
+        ]
+
+        jest.spyOn(userService, 'searchUser').mockImplementation(
+            ({ id }) =>
+                new Promise((resolve) => {
+                    resolve(listUser.filter((user) => user.id.includes(id)))
+                }) as any
+        )
+
+        expect(
+            await userController.search(
+                {
+                    query: {
+                        search: key,
+                    },
+                },
+                {
+                    json: (data) => data,
+                }
+            )
+        ).toEqual(listUser.filter((user) => user.id.includes(key)))
+    })
+})
